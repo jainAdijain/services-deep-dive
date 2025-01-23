04810563cae79575dd6e6ff95f2b23b2bbb71176
jainAdijain <jain.adityajpr1990@gmail.com>
2025-01-23 13:26:43 +0530

    - added custome injection tokens, - added the call for both the tokens in constructor as well as with inject function

---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,29 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { Task, TaskStatus } from './task.model';
+import { LoggingService } from '../logging.service';
+
+// @Injectable({ providedIn: 'root' })
+export class TasksService {
+  private tasks = signal<Task[]>([]);
+  allTasks = this.tasks.asReadonly();
+
+  private logService = inject(LoggingService);
+
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+    this.logService.log('Added Task with title ' + taskData.title);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
+  }
+}
